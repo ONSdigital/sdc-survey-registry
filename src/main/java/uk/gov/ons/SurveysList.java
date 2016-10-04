59101686ca2259d6c4196e0efde63ae7d32002eb
David Carboni <david@carboni.io>
2016-10-04 15:23:11 +0100

    Moved lazy-loading of survey data into the main controller.

--- src/main/java/uk/gov/ons/SurveysList.java
+++ src/main/java/uk/gov/ons/SurveysList.java
@@ -1,7 +1,7 @@
 package uk.gov.ons;
 
-import com.github.davidcarboni.ResourceUtils;
 import com.google.gson.Gson;
+import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import uk.gov.ons.json.Detail;
@@ -10,11 +10,17 @@ import uk.gov.ons.json.Survey;
 import uk.gov.ons.json.Surveys;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class SurveysList {
+
+    static String URL = "https://www.ons.gov.uk/surveys/informationforbusinesses/businesssurveys/staticlist/data?size=60";
+    static Surveys surveys;
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public static class ResourceNotFoundException extends RuntimeException {
@@ -25,7 +31,7 @@ public class SurveysList {
     @ResponseBody
     List<Summary> list() throws IOException {
         List<Summary> result = new ArrayList<>();
-        Surveys surveys = SurveysJson.getJson();
+        Surveys surveys = getJson();
         if (surveys != null) {
             for (Survey survey : surveys.result.results) {
                 result.add(new Summary(survey));
@@ -40,7 +46,7 @@ public class SurveysList {
     public
     @ResponseBody
     Detail detail(@PathVariable String reference) throws IOException {
-        Surveys surveys = SurveysJson.getJson();
+        Surveys surveys = getJson();
         if (surveys != null) {
             for (Survey survey : surveys.result.results) {
                 Summary summary = new Summary(survey);
@@ -52,6 +58,31 @@ public class SurveysList {
 
         // Not found;
         throw new ResourceNotFoundException();
+    }
+
+    static Surveys getJson() {
+
+        if (surveys == null) {
+
+
+            try {
+                java.net.URL url = new URL(URL);
+                URLConnection urlConnection = url.openConnection();
+                // We need to add a valid user agent, or we get a 403 from the server
+                urlConnection.addRequestProperty("User-Agent", "Mozilla/4.0");
+                try (InputStream inputStream = urlConnection.getInputStream()) {
+                    String json = IOUtils.toString(inputStream, "UTF-8");
+                    surveys = new Gson().fromJson(json, Surveys.class);
+                } catch (IOException e) {
+                    System.out.println("Error getting json data from " + url + ". Will try again on the next request.");
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return surveys;
     }
 
 }
