aeceb5830376574e318f5ac27e1a83d11448ddda
David Carboni <david@carboni.io>
2016-10-04 15:17:45 +0100

    Added a lazy-loading retry and tweaked the URN to 'survey'

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import json
 import re
 import requests
 
-
 app = Flask(__name__)
 
 # Enable cross-origin requests
@@ -17,10 +16,10 @@ registry = []
 @app.route('/', methods=['GET'])
 def info():
     result = []
-    for survey in registry:
+    for survey in get_registry():
         # ref_link = "https://sdc-survey-registry-python.herokuapp.com/" + survey["reference"]
         # urn_link =
-        # "https://sdc-survey-registry-python.herokuapp.com/urn:uk.gov.ons.surveys:id:ru:" +
+        # "https://sdc-survey-registry-python.herokuapp.com/urn:uk.gov.ons.surveys:id:survey:" +
         # survey["reference"]
         link = "/" + survey["reference"]
         # result.append({"name": survey["name"], "reference": survey["reference"], "urn": survey["urn"],
@@ -31,7 +30,7 @@ def info():
 
 @app.route('/<reference>', methods=['GET'])
 def login(reference):
-    for survey in registry:
+    for survey in get_registry():
         if survey["reference"] == reference or survey["urn"] == reference:
             return jsonify(survey)
 
@@ -41,31 +40,37 @@ def login(reference):
     return resp
 
 
-def get_json():
-    url = "https://www.ons.gov.uk/surveys/informationforbusinesses/businesssurveys/staticlist/data?size=60"
-    response = requests.get(url)
-    return response.json()
+def get_registry():
+    global registry
+
+    if len(registry) == 0:
+        url = "https://www.ons.gov.uk/surveys/informationforbusinesses/businesssurveys/staticlist/data?size=100"
+        response = requests.get(url)
+        data = response.json()
+        print(data)
+        results = data["result"]["results"]
+        for survey in results:
+            title = survey["description"]["title"]
+            if "releaseDate" in survey["description"]:
+                releaseDate = survey["description"]["releaseDate"]
+
+            # Create an acronym reference for this survey
+            acronym = ""
+            for e in re.split("\W+", title):
+                if len(e) > 0:
+                    acronym += e[0]
+
+            registry.append({
+                "name": title,
+                "urn": "urn:uk.gov.ons.surveys:id:survey:" + acronym,
+                "reference": acronym,
+                "releaseDate": releaseDate
+            })
+
+    return registry
 
 
 if __name__ == '__main__':
-
-    data = get_json()
-    print(data)
-    results = data["result"]["results"]
-    for survey in results:
-        title = survey["description"]["title"]
-        if "releaseDate" in survey["description"]:
-            releaseDate = survey["description"]["releaseDate"]
-
-        # Create an acronym reference for this survey
-        acronym = ""
-        for e in re.split("\W+", title):
-            if len(e) > 0:
-                acronym += e[0]
-
-        registry.append({"name": title, "urn": "urn:uk.gov.ons.surveys:id:ru:"+acronym, "reference": acronym, "releaseDate": releaseDate})
-
     # Start server
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
-
