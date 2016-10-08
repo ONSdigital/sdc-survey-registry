f40a175d1697d560ffa12748c80763394a80767e
David Carboni <david@carboni.io>
2016-10-08 10:50:39 +0100

    Updated to use mux and to serve dummy data for both / and /{reference}

--- app.go
+++ app.go
@@ -5,20 +5,35 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func home(w http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(w, "Hi there, I love surveys!")
+}
+
+func survey(w http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	reference := vars["reference"]
+	fmt.Fprintf(w, "Hi there, I love %s!", reference)
 }
 
 func main() {
 
+	// Configuration
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8000"
-		log.Print("$PORT set to " + port)
+		log.Print("$PORT defaulted to " + port)
 	}
 
-	http.HandleFunc("/", handler)
+	// Mux
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/{reference}", survey)
+	http.Handle("/", r)
+
+	// Run
 	http.ListenAndServe(":"+port, nil)
 }
